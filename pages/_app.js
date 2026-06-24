a2a04aa2b32838d925c952d41616543f682afd62
asiimwe3 <derickasiimwe849@gmail.com>
2026-06-24 15:52:09 +0300

    feat: add global WhatsApp floating button

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import '../styles/globals.css'
+import WhatsAppButton from '../components/WhatsAppButton'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      {/* Floating WhatsApp button on every page */}
+      <WhatsAppButton />
+    </>
+  )
 }
